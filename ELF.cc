1004add9b22ce6626e530d7d3a356f1294c87431
soraxksa <soraxksa@gmail.com>
2021-07-09 01:59:23 +0300

    ELF.h ELF.cc but not done yet

---
+++ ELF.cc
@@ -0,0 +1,4 @@
+#include "ELF.h"
+
+
+
--- memory.cc
+++ memory.cc
@@ -1,9 +1,11 @@
 #include "memory.h"
 
 
-void dump_file(const char*filename){
+
+
+void dump_file(const char *filename){
 	
-	FILE *fd = fopen(filename, "r");
+	FILE *fd = fopen(filename, "rb");
 	char buf[1024];
 	int addr = 0;
 	int buf_size;
@@ -18,25 +20,25 @@ void dump_file(const char*filename){
 
 void dump(const char *data_buffer, size_t length, uint64_t addr = 0) 
 {   
-	unsigned char byte;
-	unsigned int i, j;
-	for(i=0; i < length; i++) 
+	for(int i=0; i < length; i++) 
 	{
 		if(i%16 == 0)
 		{
 			printf("0x%016x| ", addr);
 			addr+=16;
 		}
-		byte = data_buffer[i];
+
+		unsigned char byte = data_buffer[i];
 		printf("%02x ", byte);
+
 		if(((i%16)==15) || (i==length-1)) 
 		{         
-			for(j=0; j < 15-(i%16); j++)
+			for(int j=0; j < 15-(i%16); j++)
 				printf("   ");
 
 			printf("| ");
 
-			for(j=(i-(i%16)); j <= i; j++) 
+			for(int j=(i-(i%16)); j <= i; j++) 
 			{
 				byte = data_buffer[j];
 
@@ -51,5 +53,28 @@ void dump(const char *data_buffer, size_t length, uint64_t addr = 0)
 	}  
 }  
 
+
+char* file_to_bin(const char *filename)
+{
+	FILE *fd = fopen(filename, "rb");
+	if(fd == NULL)
+	{
+		printf("ERROR: could not open file:%s. in function file_to_bin()\n", filename);
+	}
+
+	size_t buf_size = 1024;
+	size_t buf_used = 0;
+
+	char *buf = malloc(buf_size);
+	char *buf_tmp = buf;
+
+	while( (buf_used = fread(buf_tmp, 1, 1024, fd)) == 1024)
+	{
+		buf_end += buf_used;
+		if(buf_end - buf + 12
+	}
+
+
+}
 
 
